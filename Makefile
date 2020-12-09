f5c9a979ec1d3f23c9479cc103f6835b311f2f6b
Alexey Rodin <cher.alex97@gmail.com>
2020-12-09 13:28:06 +0300

    Add Webpack

--- Makefile
+++ Makefile
@@ -4,5 +4,11 @@ install:
 lint:
 	npx eslint .
 
+develop:
+	npx webpack-dev-server
+
+build:
+	npm run build
+
 deploy:
 	npx vercel
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  context: path.resolve(__dirname, 'src'),
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: 'template.html',
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+};
