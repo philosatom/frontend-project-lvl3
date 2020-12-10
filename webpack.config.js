8c77c7f72f6f7b9ed3dca8245d04fe07ed675349
Alexey Rodin <cher.alex97@gmail.com>
2020-12-10 16:31:46 +0300

    Modify Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,12 @@
-const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
-  context: path.join(__dirname, 'src'),
-  entry: './index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-  },
   plugins: [
     new HTMLWebpackPlugin({
-      template: './template.html',
+      template: 'src/template.html',
     }),
-    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
   ],
   module: {
@@ -24,5 +16,8 @@ module.exports = {
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
     ],
+  },
+  devServer: {
+    open: true,
   },
 };
