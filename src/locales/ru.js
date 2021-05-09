074e3d416f19f026adcda1ac1301bfb4c0e023a3
Alexey Rodin <cher.alex97@gmail.com>
2021-05-09 18:10:43 +0300

    Add russian language

---
+++ src/locales/ru.js
@@ -0,0 +1,20 @@
+export default {
+  translation: {
+    form: {
+      messages: {
+        errors: {
+          validation: {
+            url: 'Ссылка должна быть валидным URL',
+            uniqueness: 'RSS уже существует',
+          },
+          rss: 'Ресурс не содержит валидный RSS',
+          network: 'Ошибка сети',
+        },
+        success: 'RSS успешно загружен',
+      },
+    },
+    feeds: 'Фиды',
+    posts: 'Посты',
+    view: 'Просмотр',
+  },
+};
